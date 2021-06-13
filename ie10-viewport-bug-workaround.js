31a142dc01fd4c76ab0aeb4bccb5c44c99d538cc
VRK <kvijayraju@gmail.com>
2021-06-13 08:09:10 +0530

    test

--- ie10-viewport-bug-workaround.js
+++ ie10-viewport-bug-workaround.js
@@ -3,7 +3,7 @@
  * Copyright 2014-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
-
+// test
 // See the Getting Started docs for more information:
 // http://getbootstrap.com/getting-started/#support-ie10-width
 
